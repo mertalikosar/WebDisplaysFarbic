a2c6d07b14fe3a45f84788ef75b5de45d30a6633
mertalikosar <mertalikosar1903@gmail.com>
2026-08-07 10:15:45 +0300

    Delete src/main directory

--- src/main/java/net/montoyo/wd/item/ItemScreenConfigurator.java
+++
@@ -1,9 +0,0 @@
-package net.montoyo.wd.item;
-
-import net.minecraft.world.item.Item;
-
-public class ItemScreenConfigurator extends Item {
-    public ItemScreenConfigurator(Properties properties) {
-        super(properties);
-    }
-}
